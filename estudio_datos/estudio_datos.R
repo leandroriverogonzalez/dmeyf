394106de2615639e408b214f7d007a570dc054ee
leandroriverogonzalez <leandroriverogonzalez@gmail.com>
2021-11-16 00:46:12 +0000

    agrego analisis de datos

--- estudio_datos/estudio_datos.R
+++ estudio_datos/estudio_datos.R
@@ -24,3 +24,18 @@ datos_freq$mes_char <- as.character(datos_freq$foto_mes)
 
 ggplot(datos_freq[datos_freq$clase_ternaria!='CONTINUA',], aes(fill=clase_ternaria, y=Freq, x=mes_char)) + 
   geom_bar(position="fill", stat="identity") + scale_x_discrete(guide = guide_axis(angle = 90))
+
+
+ggplot(datos_freq[datos_freq$clase_ternaria=='BAJA+2',], aes(fill=clase_ternaria, y=Freq, x=mes_char)) + scale_x_discrete(guide = guide_axis(angle = 90))
+
+table(dataset$clase_ternaria, dataset$foto_mes)
+
+dataf_tabla <- as.data.frame(table(dataset[dataset$foto_mes<202012]$clase_ternaria, dataset[dataset$foto_mes<202012]$foto_mes))
+
+ggplot(dataf_tabla[dataf_tabla$Var1!='CONTINUA',],                                      # Grouped barplot using ggplot2
+       aes(x = Var2,
+           y = Freq,
+           fill = Var1)) +
+  geom_bar(stat = "identity",
+           position = "dodge")  + scale_x_discrete(guide = guide_axis(angle = 90))
+
