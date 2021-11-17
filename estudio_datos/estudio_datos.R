c2c2d1d89d5af19051eb9ff1250b209ee5d28f72
leandroriverogonzalez <leandroriverogonzalez@gmail.com>
2021-11-17 00:27:23 +0000

    agrego tanto en el codigo como en los pdf los graficos de todos las clases juntas incluyendo y no incluyendo los outliers

--- estudio_datos/estudio_datos.R
+++ estudio_datos/estudio_datos.R
@@ -119,33 +119,62 @@ for(variable_ahora in variables_estudio){
   }, error=function(e){print(variable_ahora)})
 }
 library(plyr)
-
-
+################################################
+#Lo copado esta acá
 dataset_wfb2 <- dataset[dataset$clase_ternaria=='BAJA+2']
-dataset_wfb2_v2 <- rbind(dataset_wfb2, dataset_final)
+dataset_wfb2 <- dataset_wfb2[dataset_wfb2$foto_mes<202012]
+
+dataset_wfcont <- dataset[dataset$clase_ternaria=='CONTINUA']
+dataset_wfcont <- dataset_wfcont[dataset_wfcont$foto_mes<202012]
+
+dataset_final <- dataset[dataset$foto_mes>202011]
+dataset_final <- dataset_final[dataset_final$clase_ternaria!='BAJA+1']
+
+dataset_cfb2 <- rbind(dataset_wfb2, dataset_final)
+
+dataset_cfb2cont <- rbind(dataset_cfb2, dataset_wfcont)
+
+dataset_cfb2cont[dataset_cfb2cont$clase_ternaria=='']$clase_ternaria = 'nosesabe'
 
 
-for(variable_ahora in variables_estudio){
+variable_ahora <- 'ctrx_quarter'
+q = c(.25, .5, .75)
+evaluo <- dataset_cfb2cont %>%
+  group_by(foto_mes) %>%
+  summarize(quant25 = quantile(eval(as.symbol(variable_ahora)), probs = q[1]), 
+            quant50 = quantile(eval(as.symbol(variable_ahora)), probs = q[2]),
+            quant75 = quantile(eval(as.symbol(variable_ahora)), probs = q[3]))
+
+
+ggplot(dataset_cfb2cont, aes(x=as.factor(foto_mes), y=eval(as.symbol(variable_ahora)), fill=clase_ternaria)) + ylim(min(evaluo$quant25) - (max(evaluo$quant75) - min(evaluo$quant25))/10, max(evaluo$quant75) + (max(evaluo$quant75) - min(evaluo$quant25))/10) +
+  geom_boxplot(outlier.shape=NA) +
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
+  labs(x = "Fecha", y = paste(variable_ahora))
+variables_estudio <- colnames(dataset)[2:length(colnames(dataset))-1]
+
+for(variable_ahora in variables_estudio[94:length(variables_estudio)]){
   tryCatch({
-    evaluo <- dataset_wfb2_v2 %>%
-      group_by(foto_mes) %>% 
+    evaluo <- dataset_cfb2cont %>%
+      group_by(foto_mes) %>%
       summarize(quant25 = quantile(eval(as.symbol(variable_ahora)), probs = q[1]), 
                 quant50 = quantile(eval(as.symbol(variable_ahora)), probs = q[2]),
-                quant75 = quantile(eval(as.symbol(variable_ahora)), probs = q[3]), na.rm = TRUE)
-    if(sd(evaluo$quant25)==0 & sd(evaluo$quant75)==0){
-      ggplot(data = dataset_wfb2_v2, aes(x=as.factor(foto_mes),y=eval(as.symbol(variable_ahora)))) +
-        scale_x_discrete(guide = guide_axis(angle = 90)) +
-        labs(x = "Fecha", y = paste(variable_ahora))
-      ggsave(paste0("/home/leandroriverogonzalez/dmeyf/estudio_datos/Bajamas2conultimosmeses/",variable_ahora,"_histogram_per_month.pdf"))
-    }else{
-      ggplot(data = dataset_wfb2_v2, aes(x=as.factor(foto_mes),y=eval(as.symbol(variable_ahora))))+geom_boxplot(outlier.shape=NA)+ ylim(min(evaluo$quant25) - (max(evaluo$quant75) - min(evaluo$quant25))/10, max(evaluo$quant75) + (max(evaluo$quant75) - min(evaluo$quant25))/10) +
-        scale_x_discrete(guide = guide_axis(angle = 90)) +
-        labs(x = "Fecha", y = paste(variable_ahora))
-      ggsave(paste0("/home/leandroriverogonzalez/dmeyf/estudio_datos/Bajamas2conultimosmeses/",variable_ahora,"_histogram_per_month.pdf"))
-    }
+                quant75 = quantile(eval(as.symbol(variable_ahora)), probs = q[3]))
+    
+    
+    ggplot(dataset_cfb2cont, aes(x=as.factor(foto_mes), y=eval(as.symbol(variable_ahora)), fill=clase_ternaria)) + ylim(min(evaluo$quant25) - (max(evaluo$quant75) - min(evaluo$quant25))/10, max(evaluo$quant75) + (max(evaluo$quant75) - min(evaluo$quant25))/10) +
+      geom_boxplot(outlier.shape=NA) +
+      scale_x_discrete(guide = guide_axis(angle = 90)) +
+      labs(x = "Fecha", y = paste(variable_ahora))
+    ggsave(paste0("/home/leandroriverogonzalez/dmeyf/estudio_datos/Todossinoutliers/",variable_ahora,"_histogram_per_month.pdf"))
+    ggplot(dataset_cfb2cont, aes(x=as.factor(foto_mes), y=eval(as.symbol(variable_ahora)), fill=clase_ternaria)) + #ylim(min(evaluo$quant25) - (max(evaluo$quant75) - min(evaluo$quant25))/10, max(evaluo$quant75) + (max(evaluo$quant75) - min(evaluo$quant25))/10) +
+      geom_boxplot(outlier.shape=NA) +
+      scale_x_discrete(guide = guide_axis(angle = 90)) +
+      labs(x = "Fecha", y = paste(variable_ahora))
+    ggsave(paste0("/home/leandroriverogonzalez/dmeyf/estudio_datos/Todosconoutliers/",variable_ahora,"_histogram_per_month.pdf"))
+    
   }, error=function(e){print(variable_ahora)})
 }
-
+######################################
 dataset_wfcont_v2 <- rbind(dataset_wfcont, dataset_final)
 
 for(variable_ahora in variables_estudio){
